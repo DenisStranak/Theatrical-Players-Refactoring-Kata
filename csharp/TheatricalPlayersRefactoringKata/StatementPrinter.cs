705bf166a7fda103299d21dfd6f1f3e8e6f7f77f
Denis Straňák <492616@mail.muni.cz>
2024-04-08 12:51:28 +0200

    d

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -4,61 +4,61 @@ using System.Globalization;
 
 namespace TheatricalPlayersRefactoringKata
 {
-    // public class StatementResult
-    // {
-    //     public string Customer { get; set; }
-    //     public List<Play> Plays { get; set; }
-    //     public List<Performance> Performances { get; set; }
-    //     public int TotalAmount { get; set; }
-    //     public int VolumeCredits { get; set; }
 
-    // }
 
     public class StatementPrinter
     {
         public string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
-            var totalAmount = 0;
+            var totalPrice = 0;
             var volumeCredits = 0;
             var result = string.Format("Statement for {0}\n", invoice.Customer);
             CultureInfo cultureInfo = new CultureInfo("en-US");
 
-            foreach (var perf in invoice.Performances)
+            foreach (var performance in invoice.Performances)
             {
-                var play = plays[perf.PlayID];
-                var thisAmount = 0;
-                switch (play.Type)
-                {
-                    case "tragedy":
-                        thisAmount = 40000;
-                        if (perf.Audience > 30)
-                        {
-                            thisAmount += 1000 * (perf.Audience - 30);
-                        }
-                        break;
-                    case "comedy":
-                        thisAmount = 30000;
-                        if (perf.Audience > 20)
-                        {
-                            thisAmount += 10000 + 500 * (perf.Audience - 20);
-                        }
-                        thisAmount += 300 * perf.Audience;
-                        break;
-                    default:
-                        throw new Exception("unknown type: " + play.Type);
-                }
+                Play play;
+                int performancePrice;
+                CalculatePlayPrice(plays, performance, out play, out performancePrice);
                 // add volume credits
-                volumeCredits += Math.Max(perf.Audience - 30, 0);
+                volumeCredits += Math.Max(performance.Audience - 30, 0);
                 // add extra credit for every ten comedy attendees
-                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)performance.Audience / 5);
 
                 // print line for this order
-                result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
-                totalAmount += thisAmount;
+                result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(performancePrice / 100), performance.Audience);
+                totalPrice += performancePrice;
             }
-            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
+            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalPrice / 100));
             result += String.Format("You earned {0} credits\n", volumeCredits);
+
             return result;
+        }
+
+        private static void CalculatePlayPrice(Dictionary<string, Play> plays, Performance performance, out Play play, out int playPrice)
+        {
+            play = plays[performance.PlayID];
+            playPrice = 0;
+            switch (play.Type)
+            {
+                case "tragedy":
+                    playPrice = 40000;
+                    if (performance.Audience > 30)
+                    {
+                        playPrice += 1000 * (performance.Audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    playPrice = 30000;
+                    if (performance.Audience > 20)
+                    {
+                        playPrice += 10000 + 500 * (performance.Audience - 20);
+                    }
+                    playPrice += 300 * performance.Audience;
+                    break;
+                default:
+                    throw new Exception("unknown type: " + play.Type);
+            }
         }
     }
 }
