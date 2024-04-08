98b9f843206523f33564712d4f03630c84121c05
Denis Straňák <492616@mail.muni.cz>
2024-04-08 13:03:27 +0200

    vcv

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -4,41 +4,53 @@ using System.Globalization;
 
 namespace TheatricalPlayersRefactoringKata
 {
-
-
     public class StatementPrinter
     {
         public string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
-            var totalPrice = 0;
-            var volumeCredits = 0;
-            var result = string.Format("Statement for {0}\n", invoice.Customer);
+            var (totalPrice, volumeCredits) = CalculateTotalPrice(invoice, plays);
             CultureInfo cultureInfo = new CultureInfo("en-US");
+
+            string result = string.Format("Statement for {0}\n", invoice.Customer);
 
             foreach (var performance in invoice.Performances)
             {
-                Play play;
-                int performancePrice;
-                CalculatePlayPrice(plays, performance, out play, out performancePrice);
-                // add volume credits
-                volumeCredits += Math.Max(performance.Audience - 30, 0);
-                // add extra credit for every ten comedy attendees
-                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)performance.Audience / 5);
+                var (play, performancePrice) = CalculatePlayPrice(plays, performance);
 
-                // print line for this order
+                // Print line for this order
                 result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(performancePrice / 100), performance.Audience);
-                totalPrice += performancePrice;
             }
+
             result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalPrice / 100));
             result += String.Format("You earned {0} credits\n", volumeCredits);
 
             return result;
         }
 
-        private static void CalculatePlayPrice(Dictionary<string, Play> plays, Performance performance, out Play play, out int playPrice)
+        private static (int, int) CalculateTotalPrice(Invoice invoice, Dictionary<string, Play> plays)
         {
-            play = plays[performance.PlayID];
-            playPrice = 0;
+            int totalPrice = 0;
+            int volumeCredits = 0;
+
+            foreach (var performance in invoice.Performances)
+            {
+                var (play, performancePrice) = CalculatePlayPrice(plays, performance);
+
+                // Add volume credits
+                volumeCredits += Math.Max(performance.Audience - 30, 0);
+                // Add extra credit for every ten comedy attendees
+                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)performance.Audience / 5);
+
+                totalPrice += performancePrice;
+            }
+
+            return (totalPrice, volumeCredits);
+        }
+
+        private static (Play, int) CalculatePlayPrice(Dictionary<string, Play> plays, Performance performance)
+        {
+            var play = plays[performance.PlayID];
+            int playPrice = 0;
             switch (play.Type)
             {
                 case "tragedy":
@@ -59,6 +71,8 @@ namespace TheatricalPlayersRefactoringKata
                 default:
                     throw new Exception("unknown type: " + play.Type);
             }
+
+            return (play, playPrice);
         }
     }
 }
